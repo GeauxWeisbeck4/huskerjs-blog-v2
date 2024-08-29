a686727907042912a07563e8cd9deeef3cc1991d
GeauxWeisbeck4 <andrew.weisbeck@gmail.com>
2024-08-28 23:20:22 -0400

    Initial commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'HuskerJS';
+export const SITE_DESCRIPTION = 'There is no place Like Nebraska';
